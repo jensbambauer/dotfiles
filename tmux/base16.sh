7822a9fb5620943809875503c05ece4cec9ee688
Nick Nisi <nick@nisi.org>
2018-04-04 11:26:07 -0500

    wip

---
+++ tmux/base16.sh
@@ -0,0 +1,33 @@
+# Base16 Styling Guidelines:
+# base00 - Default Background
+# base01 - Lighter Background (Used for status bars)
+# base02 - Selection Background
+# base03 - Comments, Invisibles, Line Highlighting
+# base04 - Dark Foreground (Used for status bars)
+# base05 - Default Foreground, Caret, Delimiters, Operators
+# base06 - Light Foreground (Not often used)
+# base07 - Light Background (Not often used)
+# base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+# base09 - Integers, Boolean, Constants, XML Attributes, Markup Link Url
+# base0A - Classes, Markup Bold, Search Text Background
+# base0B - Strings, Inherited Class, Markup Code, Diff Inserted
+# base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
+# base0D - Functions, Methods, Attribute IDs, Headings
+# base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
+
+base00=default
+base01=colour18
+base02=colour19
+base03=
+base04=colour20
+base05=colour7
+base06=
+base07=
+base08=colour01
+base09=
+base0A=colour3
+base0B=colour2
+base0C=
+base0D=
+base0E=
+base0F=
--- tmux/theme.sh
+++ tmux/theme.sh
@@ -1,16 +1,11 @@
 #### COLOUR
 
+tm_color_background=colour18
 tm_color_active=colour32
 tm_color_inactive=colour241
 tm_color_feature=colour206
 tm_color_music=colour215
 tm_active_border_color=colour240
-
-# separators
-tm_separator_left_bold="◀"
-tm_separator_left_thin="❮"
-tm_separator_right_bold="▶"
-tm_separator_right_thin="❯"
 
 set -g status-left-length 32
 set -g status-right-length 150
@@ -19,7 +14,7 @@ set -g status-interval 5
 # default statusbar colors
 # set-option -g status-bg colour0
 set-option -g status-fg $tm_color_active
-set-option -g status-bg default
+set-option -g status-bg $tm_color_background
 set-option -g status-attr default
 
 # default window title colors
@@ -49,11 +44,11 @@ set-window-option -g clock-mode-colour $tm_color_active
 
 # tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/tunes.scpt | cut -c 1-50)"
 tm_tunes="#[fg=$tm_color_music]#(osascript -l JavaScript ~/.dotfiles/applescripts/tunes.js)"
-tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
+# tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
 
 tm_date="#[fg=$tm_color_inactive] %R %d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="#[fg=$tm_color_feature,bold]#S"
 
-set -g status-left $tm_session_name' '
-set -g status-right $tm_tunes' '$tm_date' '$tm_host
+set -g status-left "$tm_session_name "
+set -g status-right "$tm_tunes $tm_date $tm_host"
